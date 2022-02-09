0477aaa68440348f8b7093f387dd957ef85055e0
김현희 <tta_bi@naver.com>
2022-02-09 14:58:22 +0900

    수정

--- phonebook/api/contact/contact.py
+++ phonebook/api/contact/contact.py
@@ -1,14 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from django.views import View
 from django.http import JsonResponse
 
 from phonebook.models import Contacts
 from phonebook.serializer import ContactsSerializer
 
-class Contact(View) :
+class Contact(APIView):
     
     def get(self, request):
         
-        # request의 기능 중 메쏘드 이름['파라미터'] 방식으로 파라미터값 추출
+        # request의 기능중, 메쏘드 이름 ['파라미터'] 방식으로 파라미터값 추출.
         
         search_name = request.GET['name']
         
@@ -16,21 +19,22 @@ class Contact(View) :
         
         serialized = ContactsSerializer(db_contact)
         
-        return JsonResponse ({
-            'code' : 200,
-            'message' : '연락처 - GET 테스트',
-            'data' : {
-                'contact' : serialized.data
+        return Response({
+            'code': 200,
+            'message': '연락처 - GET 테스트',
+            'data': {
+                'contact': serialized.data
             }
         })
         
     def post(self, request):
         
         
-        return JsonResponse ({
-            'code' : 200,
-            'message' : '연락처 - POST 테스트',
-            'data' : {
-                '임시항목 ' : request.POST['email'],
+        
+        return JsonResponse({
+            'code': 200,
+            'mesasge': '연락처-POST 테스팅',
+            'data': {
+                '임시항목': request.POST['email'],
             }
         })
