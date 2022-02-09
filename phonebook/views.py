7f7c8b5a8f6cb88bf3f3bdcd9feba380da1d111b
김현희 <tta_bi@naver.com>
2022-02-09 12:08:31 +0900

    기본적인 http 응답 내려주기

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def test_home(request):
+    content = '<h1>여기는 홈 화면입니다. </h1>' # HTML 태크를 리턴
+    return HttpResponse(content)
