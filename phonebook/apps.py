6d436abf80b1c877cf961349f641771e2d9d3402
김현희 <tta_bi@naver.com>
2022-02-09 10:28:09 +0900

    장고 세팅 완료

--- phonebook/apps.py
+++ phonebook/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PhonbookConfig(AppConfig):
+class PhonebookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'phonbook'
+    name = 'phonebook'
